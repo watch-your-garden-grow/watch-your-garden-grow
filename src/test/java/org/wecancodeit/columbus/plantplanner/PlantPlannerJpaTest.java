b0772ddcfc458d83e38c1af4dab925fe59652e3e
cooksey3 <chris@cookseys.org>
2018-04-02 10:41:42 -0400

    Ran and passed Jpa Test

--- src/test/java/org/wecancodeit/columbus/plantplanner/PlantPlannerJpaTest.java
+++ src/test/java/org/wecancodeit/columbus/plantplanner/PlantPlannerJpaTest.java
@@ -2,15 +2,14 @@ package org.wecancodeit.columbus.plantplanner;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class PlantPlannerApplicationTests {
+@DataJpaTest
+public class PlantPlannerJpaTest {
 
 	@Test
-	public void contextLoads() {
-	}
-
+	public void shouldInitializeJpa() {}
+	
 }
