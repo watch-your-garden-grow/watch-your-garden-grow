d6ff028d9e1424f8699b1a1c0ec949c7b2851491
cooksey3 <chris@cookseys.org>
2018-04-02 11:57:37 -0400

    Tested to return a list of plants based on zone

--- src/main/java/org/wecancodeit/columbus/plantplanner/ListManager.java
+++ src/main/java/org/wecancodeit/columbus/plantplanner/ListManager.java
@@ -1,9 +1,22 @@
 package org.wecancodeit.columbus.plantplanner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListManager {
 
 	public boolean checkPlantViability(String checkedZone, PlantDouble checkedPlant) {
 		return checkedZone == checkedPlant.getZone();
+	}
+
+	public List<PlantDouble> returnListOfViablePlants(List<PlantDouble> plantsToTest, String plantHardinessZone) {
+		List<PlantDouble> selectedPlants = new ArrayList<>();
+		for (PlantDouble plant : plantsToTest) {
+			if (checkPlantViability(plantHardinessZone, plant)) {
+				selectedPlants.add(plant);
+			}
+		}
+		return selectedPlants;
 	}
 
 }
--- src/test/java/org/wecancodeit/columbus/plantplanner/ListManagerTest.java
+++ src/test/java/org/wecancodeit/columbus/plantplanner/ListManagerTest.java
@@ -1,17 +1,22 @@
 package org.wecancodeit.columbus.plantplanner;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
 public class ListManagerTest {
 
+	private ListManager underTest = new ListManager();
+	private PlantDouble testPlant = new PlantDouble("6a", "Tomato");
+	private PlantDouble testPlant2 = new PlantDouble("10a", "Pineapple");
+
 	@Test
 	public void shouldReturnTrueIfPlantIsInAGivenZone() {
-		ListManager underTest = new ListManager();
-		PlantDouble testPlant = new PlantDouble("6a", "Tomato");
-
 		boolean result = underTest.checkPlantViability("6a", testPlant);
 
 		assertThat(result, is(true));
@@ -19,11 +24,27 @@ public class ListManagerTest {
 
 	@Test
 	public void shouldReturnFalseIfPlantIsNotInAGivenZone() {
-		ListManager underTest = new ListManager();
-		PlantDouble testPlant2 = new PlantDouble("10a", "Pineapple");
-
 		boolean result = underTest.checkPlantViability("6a", testPlant2);
 
 		assertThat(result, is(false));
+	}
+
+	@Test
+	public void shouldCheckForZone6aAndReturnListOfTomatoAndCorn() {
+		PlantDouble testPlant3 = new PlantDouble("6a", "Corn");
+		List<PlantDouble> plantDoubles = new ArrayList<>();
+
+		plantDoubles.add(testPlant);
+		plantDoubles.add(testPlant2);
+		plantDoubles.add(testPlant3);
+
+		List<PlantDouble> result = underTest.returnListOfViablePlants(plantDoubles, "6a");
+
+		assertThat(result, containsInAnyOrder(testPlant, testPlant3));
+	}
+	
+	@Test
+	public void shouldCheckForZone1AndReturnEmptyList() {
+		
 	}
 }
