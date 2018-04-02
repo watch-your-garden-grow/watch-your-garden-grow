7c21adb6d8d528e6f671f1838288e8d603ca5db4
cooksey3 <chris@cookseys.org>
2018-04-02 10:31:35 -0400

    Starting integration tests

---
+++ src/main/java/org/wecancodeit/columbus/plantplanner/PlantPlannerApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.columbus.plantplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantPlannerApplication.class, args);
+	}
+}
---
+++ src/test/java/org/wecancodeit/columbus/plantplanner/PlantPlannerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.columbus.plantplanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlantPlannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
