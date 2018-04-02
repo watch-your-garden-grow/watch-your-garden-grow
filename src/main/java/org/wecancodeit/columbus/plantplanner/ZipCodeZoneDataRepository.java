bd098ebff415453946471a543026348d06c27e6d
David Langford <langfod@gmail.com>
2018-04-02 13:46:15 -0400

    Create initial zipcode to hardiness POJO and JPA entity
     based on github.com/waldoj/frostline and PRISM datasets to be used by
    the ww.farmsense.net api

---
+++ src/main/java/org/wecancodeit/columbus/plantplanner/ZipCodeZoneDataRepository.java
@@ -0,0 +1,7 @@
+package org.wecancodeit.columbus.plantplanner;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ZipCodeZoneDataRepository extends CrudRepository<ZipCodeZoneData, Long> {
+
+}
