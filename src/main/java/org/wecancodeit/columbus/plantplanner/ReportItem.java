c6f2478460018aa5b3c002353a384147aa9b5ee6
David Langford <langfod@gmail.com>
2018-04-09 15:47:30 -0400

    initial Report template

--- src/main/java/org/wecancodeit/columbus/plantplanner/ReportItem.java
+++ src/main/java/org/wecancodeit/columbus/plantplanner/ReportItem.java
@@ -4,22 +4,30 @@ import java.time.LocalDate;
 
 public class ReportItem {
 
-	private Plant plant;
+	private final Plant plant;
 	private LocalDate frostDate = LocalDate.of(LocalDate.now().getYear(), 4, 20);
+	private LocalDate sowDate;
 
+	public ReportItem(Plant plant, LocalDate lastFrost) {
+		this.plant = plant;
+		this.frostDate = lastFrost;
+		this.sowDate = calculateSowDate();
+	}
 	public ReportItem(Plant plant) {
 		this.plant = plant;
-
+		this.sowDate = calculateSowDate();
 	}
 
 	public Plant getPlant() {
-
 		return plant;
 	}
 
-	public LocalDate reportSowDate() {
+	private LocalDate calculateSowDate() {
 		int daysAfterFrostToPlant = plant.daysSinceLastFrost();
-
 		return frostDate.plusDays(daysAfterFrostToPlant);
+	}
+
+	public LocalDate getSowDate() {
+		return sowDate;
 	}
 }
