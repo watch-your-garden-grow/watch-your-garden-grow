f09618c658c71178b45e65b444600a6c1d144a3d
David Langford <langfod@gmail.com>
2018-04-10 10:23:13 -0400

    add controller and template for sample/testing plan html page.

--- src/main/java/org/wecancodeit/columbus/plantplanner/PlantPlanController.java
+++ src/main/java/org/wecancodeit/columbus/plantplanner/PlantPlanController.java
@@ -1,11 +1,14 @@
 package org.wecancodeit.columbus.plantplanner;
 
+import java.util.Comparator;
+
 import javax.annotation.Resource;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -57,4 +60,11 @@ public class PlantPlanController {
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
+	@GetMapping(value = "/plantplan/{plantPlanId}")
+	public String presetPlantPlan(@PathVariable("plantPlanId") Long plantPlanId, Model model) {
+		PlantPlan plantPlan = plantPlanRepository.findOne(plantPlanId);
+		model.addAttribute("plantPlanItems" , plantPlan.getPlantPlanItems());	
+		model.addAttribute("plantNameComp", Comparator.comparing(PlantPlanItem::getPlantName).reversed());
+		return "plantplan";
+	}
 }
