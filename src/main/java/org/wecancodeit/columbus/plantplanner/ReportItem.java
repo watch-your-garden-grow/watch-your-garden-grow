9aebadb73bca528a8f2e1c0d43cb987ccc449475
David Langford <langfod@gmail.com>
2018-04-11 11:29:28 -0400

    calculateUserSowDate() to move planting date based on requested weekday

--- src/main/java/org/wecancodeit/columbus/plantplanner/ReportItem.java
+++ src/main/java/org/wecancodeit/columbus/plantplanner/ReportItem.java
@@ -1,33 +1,55 @@
 package org.wecancodeit.columbus.plantplanner;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class ReportItem {
 
 	private final Plant plant;
-	private LocalDate frostDate = LocalDate.of(LocalDate.now().getYear(), 4, 20);
-	private LocalDate sowDate;
+	private LocalDate lastFrost = LocalDate.of(LocalDate.now().getYear(), 4, 20);
+	private LocalDate earliestSowDate;
+	private final DayOfWeek defaultWorkDay = DayOfWeek.SATURDAY;
 
 	public ReportItem(Plant plant, LocalDate lastFrost) {
 		this.plant = plant;
-		this.frostDate = lastFrost;
-		this.sowDate = calculateSowDate();
+		this.lastFrost = lastFrost;
+		this.earliestSowDate = calculateEarliestSowDateSowDate();
 	}
+
 	public ReportItem(Plant plant) {
 		this.plant = plant;
-		this.sowDate = calculateSowDate();
+		this.earliestSowDate = calculateEarliestSowDateSowDate();
 	}
 
 	public Plant getPlant() {
 		return plant;
 	}
 
-	private LocalDate calculateSowDate() {
+	private LocalDate calculateEarliestSowDateSowDate() {
 		int daysAfterFrostToPlant = plant.daysSinceLastFrost();
-		return frostDate.plusDays(daysAfterFrostToPlant);
+		return lastFrost.plusDays(daysAfterFrostToPlant);
 	}
 
-	public LocalDate getSowDate() {
-		return sowDate;
+	public LocalDate calculateUserSowDate() {
+		return calculateUserSowDate(this.defaultWorkDay);
+	}
+
+	public LocalDate calculateUserSowDate(DayOfWeek requestedWorkDay) {
+		LocalDate userSowDate = earliestSowDate;
+
+		DayOfWeek sowDay = earliestSowDate.getDayOfWeek();
+		int daysApart = requestedWorkDay.getValue() - sowDay.getValue();
+
+		if (daysApart < 0) {
+			userSowDate = earliestSowDate.plusDays(daysApart + 7);
+		} else if (daysApart > 0) {
+			userSowDate = earliestSowDate.plusDays(daysApart);
+		}
+
+		return userSowDate;
+	}
+
+	public LocalDate getEarliestSowDate() {
+		return earliestSowDate;
 	}
 }
--- src/test/java/org/wecancodeit/columbus/plantplanner/ReportItemTest.java
+++ src/test/java/org/wecancodeit/columbus/plantplanner/ReportItemTest.java
@@ -3,9 +3,12 @@ package org.wecancodeit.columbus.plantplanner;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 import org.junit.Test;
+import org.wecancodeit.columbus.plantplanner.Plant;
+import org.wecancodeit.columbus.plantplanner.ReportItem;
 
 public class ReportItemTest {
 	Plant dummyPlant = new Plant("Banana", "", "", 0);
@@ -20,7 +23,7 @@ public class ReportItemTest {
 
 	@Test
 	public void shouldHaveASowDate() {
-		LocalDate sowDate = underTest.getSowDate();
+		LocalDate sowDate = underTest.getEarliestSowDate();
 		assertThat(sowDate, is(LocalDate.of(LocalDate.now().getYear(), 4, 20)));
 	}
 
@@ -28,7 +31,7 @@ public class ReportItemTest {
 	public void tomatoShouldHave504asASowDate() {
 		Plant dummyTomato = new Plant("Tomato", "", "", 14);
 		ReportItem test2 = new ReportItem(dummyTomato);
-		LocalDate sowDate = test2.getSowDate();
+		LocalDate sowDate = test2.getEarliestSowDate();
 		assertThat(sowDate, is(LocalDate.of(LocalDate.now().getYear(), 5, 4)));
 	}
 
@@ -36,7 +39,42 @@ public class ReportItemTest {
 	public void cantelopeShouldHave510AsASowDate() {
 		Plant dummyCantelope = new Plant("Tomato", "", "", 20);
 		ReportItem test2 = new ReportItem(dummyCantelope);
-		LocalDate sowDate = test2.getSowDate();
+		LocalDate sowDate = test2.getEarliestSowDate();
 		assertThat(sowDate, is(LocalDate.of(LocalDate.now().getYear(), 5, 10)));
 	}
+	
+	// 420 + 20 = 510 (Thursday)
+	@Test
+	public void cantelopeShouldHave512AsUserSowDateForSaturdday() {
+		Plant dummyCantelope = new Plant("Tomato", "", "", 20);
+		ReportItem test2 = new ReportItem(dummyCantelope);
+		LocalDate sowDate = test2.calculateUserSowDate();
+		assertThat(sowDate, is(LocalDate.of(LocalDate.now().getYear(), 5, 12)));
+	}
+	
+	@Test
+	public void cantelopeShouldHave519AsSowDateGivenNegativeDifference() {
+		Plant dummyCantelope = new Plant("Tomato", "", "", 23);
+		ReportItem test2 = new ReportItem(dummyCantelope);
+		LocalDate sowDate = test2.calculateUserSowDate();
+		assertThat(sowDate, is(LocalDate.of(LocalDate.now().getYear(), 5, 19)));
+	}
+	
+	@Test
+	public void cantelopeShouldHave512AsSowDateGivenZeroDifference() {
+		Plant dummyCantelope = new Plant("Tomato", "", "", 22);
+		ReportItem test2 = new ReportItem(dummyCantelope);
+		LocalDate sowDate = test2.calculateUserSowDate();
+		assertThat(sowDate, is(LocalDate.of(LocalDate.now().getYear(), 5, 12)));
+	}
+	
+	@Test
+	public void cantelopeShouldHave532AsUserSowDateForSunday() {
+		Plant dummyCantelope = new Plant("Tomato", "", "", 20);
+		ReportItem test2 = new ReportItem(dummyCantelope);
+		LocalDate sowDate = test2.calculateUserSowDate(DayOfWeek.SUNDAY);
+		assertThat(sowDate, is(LocalDate.of(LocalDate.now().getYear(), 5, 13)));
+	}
+	
+	
 }
