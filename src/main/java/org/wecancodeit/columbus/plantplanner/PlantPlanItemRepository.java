0ea4dc41ec52fd627eb8437d1d09410ae5fbaf88
David Langford <langfod@gmail.com>
2018-04-11 15:09:43 -0400

    remove bad old comment

--- src/main/java/org/wecancodeit/columbus/plantplanner/PlantPlanItemRepository.java
+++ src/main/java/org/wecancodeit/columbus/plantplanner/PlantPlanItemRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface PlantPlanItemRepository extends CrudRepository<PlantPlanItem, Long> {
 
-	//Optional<PlantPlanItem> findByCartAndProduct(PlantPlan plantPlan, Plant plant);
 	Optional<PlantPlanItem> findByPlantPlanIdAndPlantId(Long plantPlanId, Long planttId);
 }
