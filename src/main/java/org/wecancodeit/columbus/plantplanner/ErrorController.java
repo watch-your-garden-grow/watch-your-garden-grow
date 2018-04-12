a64cb110c200882976011ab1850f0d62e513ce5b
BenWilliams <deanpbr@gmail.com>
2018-04-12 14:12:45 -0400

    error.html is created

---
+++ src/main/java/org/wecancodeit/columbus/plantplanner/ErrorController.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.columbus.plantplanner;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController {
+
+	@RequestMapping("/")
+	public void handleRequest() {
+		throw new RuntimeException("test exception");
+	}
+}
--- src/main/resources/static/js/plantImagesJs.js
+++ src/main/resources/static/js/plantImagesJs.js
@@ -1,7 +1,7 @@
 const xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
 
-
+	
 	if (xhr.readyState === 4 && xhr.status === 200) {
 		const res = JSON.parse(xhr.responseText);
 
