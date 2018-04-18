8ade71a28e4dfb666be071c2e960a99ab97c2ece
Kasey <KaseyColian@gmail.com>
2018-04-17 20:23:09 -0400

    updated site instructions display. updated home view footer to match about us page. listOfEachPlantForHardinessZone is now initially displayed as none, javascript triggers display: grid

--- src/main/resources/static/js/plantImagesJs.js
+++ src/main/resources/static/js/plantImagesJs.js
@@ -133,8 +133,11 @@ const zipCodeSubmitButton = document.querySelector('#zipCodeSubmitButton');
 zipCodeSubmitButton.addEventListener('click', function(event){
 	event.preventDefault();
 	const passedZipCode = zipCodeSubmitButton.parentElement.parentElement.querySelector('input').value;
+	passedZipCode
 	const instructions = document.querySelector('.hardinessZone')
 	instructions.style.display = 'none'
+	const sectionForAllPlantsForHardinessZone = document.querySelector('.sectionForAllPlantsForHardinessZone')
+	sectionForAllPlantsForHardinessZone.style.display = 'grid'
 	const openingImage = document.querySelector('#welcomeToPageImage')
 	openingImage.style.display = 'none'
 	xhr.open('GET', '/plants/zipcode/'+ passedZipCode, true);
